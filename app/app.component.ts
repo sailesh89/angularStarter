c5c973b9c40d955490fe27830ecea733f8c46958
Sailesh Kumar Chockalingam <SAILESH89@LIVE.COM>
2017-09-03 19:49:45 -0400

    component complete bootstrapped

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',    
+    template: `
+    <h1>This is my first angular 2 Component</h1>
+    `,
+    styles:[`
+    `]
+})
+
+export class AppComponent  {
+    constructor() { }
+    
+}
